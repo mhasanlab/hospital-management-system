b15cb053b26ce937e59e8d8d47416c3d349a7c56
Maksudur Rahman BABU <106919285+Maksudur-Rahman-BABU@users.noreply.github.com>
2022-09-18 13:02:11 +0600

    Update Employee.cs

--- HospitalAPI/Models/Class/Employee.cs
+++ HospitalAPI/Models/Class/Employee.cs
@@ -15,13 +15,21 @@ namespace HospitalAPI.Models.Class
         //Foreign Key
         [Required, ForeignKey("Department")]
         public int DepartmentId { get; set; }
+        [Required, StringLength(50)]
         public string FirstName { get; set; }
+        [Required, StringLength(50)]
         public string LastName { get; set; }
+        [Required, StringLength(50)]
         public string Country { get; set; }
+        [Required, Column(TypeName = "date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime Dob { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required, StringLength(150)]
         public string Address { get; set; }
+        [Required, StringLength(50)]
         public string Phone { get; set; }
+        [Required, StringLength(50)]
         public string Email { get; set; }
 
         //Nev
