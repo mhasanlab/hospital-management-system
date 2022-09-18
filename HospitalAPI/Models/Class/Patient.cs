bf8036540e237de600823b9844504de969edc6ef
Imam-masum <95578514+Imam-masum@users.noreply.github.com>
2022-09-18 13:18:20 +0600

    Update Patient.cs

--- HospitalAPI/Models/Class/Patient.cs
+++ HospitalAPI/Models/Class/Patient.cs
@@ -10,14 +10,26 @@ namespace HospitalAPI.Models.Class
     {
         [Key]
         public int PatientId { get; set; }
+        
+        [Required, StringLength(100),Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required, StringLength(100), Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required,Display(Name = "Patient Gender")]
         public string Gender { get; set; }
+        [Required,StringLength(300),Display(Name ="Patient Address")]
         public string Address { get; set; }
+        [Required,Display(Name ="Patient City")]
         public string City { get; set; }
-        public DateTime Dob { get; set; }
+        [Required, Column(TypeName = "date"),Display(Name ="Date Of Brith"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public string DOB { get; set; }
+        
+        [Required, StringLength(12), Display(Name = "Patient Phone No")]
         public string Phone { get; set; }
+        
+        [Required, StringLength(50)]
         public string Email { get; set; }
+        
 
         // Nev
         public virtual ICollection<Addmission> Addmissions { get; set; }
