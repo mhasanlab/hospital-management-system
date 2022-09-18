4d29bbed71123f9a61e0a5b0ff2005ab992f3c21
Md Shihab Uddin <102530834+shuvoshihab@users.noreply.github.com>
2022-09-18 13:36:08 +0600

    Update Lab.cs

--- HospitalAPI/Models/Class/Lab.cs
+++ HospitalAPI/Models/Class/Lab.cs
@@ -14,13 +14,18 @@ namespace HospitalAPI.Models.Class
         //Foreign Key
         [Required, ForeignKey("MedicalTest")]
         public int MedicalTestCode { get; set; }
+        [Required,DataType(DataType.Date),Display(Name ="Test Date"),DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
         public DateTime TestDate { get; set; }
         //Foreign Key
         [Required, ForeignKey("Patient")]
         public int PatientId { get; set; }
+        [Required,Range(3,10)]
         public decimal Height { get; set; }
+        [Required,StringLength(3,ErrorMessage = "Weight can't be more than 3 Digit")]
         public decimal Weight { get; set; }
+        [Required]
         public decimal BloodPressure { get; set; }
+        [Required, Range(2, 4), Display(Name = "Blood Group")]
         public string BloodGroup { get; set; }
 
         //Nev
